9478928886ba46ce130daed1ee5c0cd884012e55
myg465 <61230966+myg465@users.noreply.github.com>
2020-03-27 17:51:26 +0900

    Add files via upload

---
+++ jsp_0327/WebContent/js/custom.js
@@ -0,0 +1,24 @@
+function move1(obj){
+	$(obj).stop().animate({
+		"left":"600px",
+		"top":"0",
+		"border-radius":"150px"
+	});
+}
+
+function move2(obj){
+	$(obj).stop().animate({
+		"left":"300px",
+		"top":"0",
+		"border-radius":"30px",
+		"opacity":"10%"
+	});
+}
+
+
+$(document).ready(function(){
+	
+	
+	
+	
+});
